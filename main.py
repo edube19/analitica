a9a57e8b1421058ef7ba07b2660a8ae36a7d0af8
edube19 <92874291+edube19@users.noreply.github.com>
2022-10-27 23:12:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 from playwright.sync_api import Playwright, sync_playwright
 import time
-
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(accept_downloads=True)
+    context = browser.new_context()
     page = context.new_page()
-    i=0
-    while i<10:
-        print(i)
-        page.goto("https://www.tripadvisor.com.pe/Hotels-g34439-Miami_Beach_Florida-Hotels.html")
-        page.click("#property_7606777")
-        i +=1
-    
+    page.goto("https://www.tripadvisor.com.pe/Hotels-g34439-Miami_Beach_Florida-Hotels.html",wait_until = 'networkidle')
+    # page.click('#lithium-root > main > nav > div > div > div.fOydb.o > a.EJavG.c._S.JlIuS > span')
+    #page.evaluate("document.querySelector('body > div.KWdaU.Za.f.e > div:nth-child(2)').remove()")
+    page.click('#global-nav-hotels')
+    # page.click("#lithium-root > main > div:nth-child(4) > div > div > div.lMwow.ZLOro > div.LeBBQ > div.dWEXi > div.wUpSI > div > div > button.bqhCp.u.z.ZIYzG.iMAUz")
+    #page.keyboard.press('Escape')
+    #page.keyboard.press('Escape')
+    # page.reload()
+    print('recargo la pagina')
+    time.sleep(3)
+    #page.click('lithium-root > main > div:nth-child(4) > div > div > div.lMwow.ZLOro > div.dErAM._Z.l._U > div.oOsqo._Z')
+    print("Completado")
+    #page.click('#lithium-root > main > div:nth-child(4) > div > div > div.zamsQ > div > div > div.LvzMf._T > div > div > div:nth-child(3) > span > span > span > div > div > div.yJIls.z.M0 > header > div > div > div > a > div')
+    page.locator('#component_7 > div > button')
+    time.sleep(5)
+    page.click('#component_7 > div > button')
 
+    time.sleep(5)
+    print('hizo click')
+    with page.expect_popup() as first:
+        page.click("#property_7606777")
+    page1 = first.value
+    page1.reload()
+    time.sleep(3)
+    nombre = page1.inner_text('#HEADING')
+    print(nombre)
+    page1.click('#HEADING')
+    page1.click('#HEADING')
+    page1.click('#HEADING')
+    page1.click('#HEADING')
+    # page.keyboard.press('Escape')
+    # page.keyboard.press('Escape')    
+
+    """ nombre = page1.evaluate("document.querySelector('#HEADING').innerText")
+    print(nombre)
+    precio1 = page1.evaluate("document.querySelector('#component_4 > div > div.premium_offers_area.offers > div:nth-child(1) > div > div.ui_column.is-4.qnqtP.P.u > div > div.WXMFC.b').innerText")
+    print('Expedia',precio1)
+    precio2 = page1.evaluate("document.querySelector('#component_4 > div > div.premium_offers_area.offers > div:nth-child(2) > div > div.ui_column.is-4.qnqtP.P.u > div > div').innerText")
+    print('Booking.com', precio2)
+    precio3 = page1.evaluate("document.querySelector('#component_4 > div > div.premium_offers_area.offers > div:nth-child(3) > div > div.ui_column.is-4.qnqtP.P.u > div > div.msiXY.b.Wa').innerText")
+    print('agoda',precio3)  """
+    for i in range(3):
+        i = i+1
+        precio = page1.inner_text('#component_4 > div > div.premium_offers_area.offers > div:nth-child(1) > div > div.ui_column.is-4.qnqtP.P.u > div > div.WXMFC.b')
+        print(precio)
 with sync_playwright() as playwright:
     run(playwright)
--- prueba.py
+++ prueba.py
@@ -1,43 +1,24 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright
 import time
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.tripadvisor.com.pe/Hotels-g34439-Miami_Beach_Florida-Hotels.html")
-    #time.sleep(3)
-    page.locator("taplc_global_nav_0 > div.global-nav-links.global-nav-bottom.ui_tabs.is-hidden-mobile > div > div.message_container.ui_column.is-1").click()
-    page.click("component_7 > div > button")
+
+    print('entro')
+    page.evaluate(('''()=>{
+            "document.querySelector('BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e').remove()"    
+            }'''))
+    #page.evaluate("document.querySelector('BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e').remove()")
     time.sleep(3)
-    #para quitar el calendario
-    try:
-        page.locator("BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e > div:nth-child(2) > div > div:nth-child(2) > div > div > div.ZxKNQ > div > div > div > div.rujDD.q.c > div:nth-child(1) > div.SWwwt.notranslate > div:nth-child(1) > div.rJYai.Vt.Z1.start.selected").click()
-        page.locator("BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e > div:nth-child(2) > div > div:nth-child(2) > div > div > div.ZxKNQ > div > div > div > div.rujDD.q.c > div:nth-child(1) > div.SWwwt.notranslate > div:nth-child(2) > div:nth-child(1)").click()
-        page.locator("body > div.KWdaU.Za.f.e > div:nth-child(2) > div > button").click()
-        print("salio del calendario")
-    except:
-        page.locator("taplc_global_nav_0 > div.global-nav-links.global-nav-bottom.ui_tabs.is-hidden-mobile > div > div.message_container.ui_column.is-1").click()
-        time.sleep(3)
-        print('DEBIO CERRAR')
-        #https://www.tripadvisor.com.pe/Hotels-g34439-oa90-Miami_Beach_Florida-Hotels.html
-        #page.click("#TRIP_PLANNER")
-        time.sleep(3)
-        print("Completado")
-        time.sleep(3)
-        #boton mostrar todo
-        #page.click("component_7 > div > button")
-        time.sleep(3)
-        print('presiono el boton')
-    # ---------------------
+    #BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e
+    """page.locator("taplc_global_nav_onpage_assets_0 > div > div").click()
+    page.locator("taplc_hsx_hotel_list_lite_dusty_hotels_combined_sponsored_ad_density_control_0").click()"""
+    print('paso la tabla')
     context.close()
     browser.close()
+    #TABLA
+    #taplc_hsx_hotel_list_lite_dusty_hotels_combined_sponsored_ad_density_control_0
 with sync_playwright() as playwright:
     run(playwright)
-#dia 6
-##BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e > div:nth-child(2) > div > div:nth-child(2) > div > div > div.ZxKNQ > div > div > div > div.rujDD.q.c > div:nth-child(1) > div.SWwwt.notranslate > div:nth-child(1) > div.rJYai.Vt.Z1.start.selected
-#dia 7
-#BODY_BLOCK_JQUERY_REFLOW > div.KWdaU.Za.f.e > div:nth-child(2) > div > div:nth-child(2) > div > div > div.ZxKNQ > div > div > div > div.rujDD.q.c > div:nth-child(1) > div.SWwwt.notranslate > div:nth-child(2) > div:nth-child(1)
-
-#salir
-#body > div.KWdaU.Za.f.e > div:nth-child(2) > div > button
-
